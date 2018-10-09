db48b7e376648683f821e8903743abe105e11792
gja10 <g.j.allsop@brighton.ac.uk>
2018-10-09 09:54:08 +0100

    added a viewport

--- core/src/com/allsopg/game/MyGdxGame.java
+++ core/src/com/allsopg/game/MyGdxGame.java
@@ -5,13 +5,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	ScreenViewport viewport;
+
+	public static final float PPM = 16;
 	
 	@Override
 	public void create () {
+		viewport = new ScreenViewport();
+		viewport.setUnitsPerPixel(1/PPM);
+		viewport.update(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
@@ -20,8 +27,9 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(viewport.getCamera().combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0,img.getWidth()/PPM,img.getHeight()/PPM);
 		batch.end();
 	}
 	
